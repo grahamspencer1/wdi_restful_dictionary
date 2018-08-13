bed0dff4aa30d7c56c01fbf072ae7a0191354501
Graham Spencer <grahamspence@gmail.com>
2018-08-13 17:39:14 -0400

    got the ball rolling on index

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,19 +1,32 @@
 class EntriesController < ApplicationController
 
   def index
+    @entries = Entry.all
   end
 
   def show
+    @entry = Entry.find(params[:id])
   end
 
   def new
+    @entry = Entry.new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def edit
+    @entry = Entry.find(params[:id])
   end
 
   def update
